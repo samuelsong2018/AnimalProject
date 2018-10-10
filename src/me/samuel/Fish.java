afa7afe3a43f293f71e6534eaadf70c32aec83f5
swimmerlap <dizzgil@aol.com>
2018-10-10 14:07:36 -0400

    New extension.

---
+++ src/me/samuel/Fish.java
@@ -0,0 +1,12 @@
+package me.samuel;
+
+public class Fish extends Animal{
+
+    public String swim(){
+        return "The fish is swimming.";
+    }
+
+    public String eat(){
+        return "The fish is sleeping.";
+    }
+}
